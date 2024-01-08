2868e5fd21815bab95defe0a7bc84adff8d29be0
Vitor <114679696+EHOVITAUM@users.noreply.github.com>
2024-01-08 14:19:58 -0300

    Delete bucket_s3/terraform directory

--- bucket_s3/terraform/s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "aqui-eh-um-label" {
-    bucket = "bucket-test-terraform-sei-la"
-
-    tags = {
-        name = var.project_name
-        enviroment = var.enviroment 
-    }
-}
-
